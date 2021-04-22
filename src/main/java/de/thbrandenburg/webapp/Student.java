a099fa0f31bbedb8ca3827dba6a65c7f77a7f64c
Johanna0708 <schulzj@th-brandenburg.de>
2021-04-22 22:55:36 +0200

    changes

---
+++ src/main/java/de/thbrandenburg/webapp/Student.java
@@ -0,0 +1,11 @@
+package de.thbrandenburg.webapp;
+
+public class Student {
+    public String vorname;
+    public String nachname;
+
+    public Student(String vorname, String nachname){
+        this.vorname = vorname;
+        this.nachname = nachname;
+    }
+}
--- src/main/java/de/thbrandenburg/webapp/WebAppApplication.java
+++ src/main/java/de/thbrandenburg/webapp/WebAppApplication.java
@@ -1,11 +1,15 @@
 package de.thbrandenburg.webapp;
+	import org.springframework.boot.SpringApplication;
+	import org.springframework.boot.autoconfigure.SpringBootApplication;
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.PostMapping;
+	import org.springframework.web.bind.annotation.RequestParam;
+	import org.springframework.web.bind.annotation.RequestBody;
+	import org.springframework.web.bind.annotation.RestController;
 
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+	import java.util.ArrayList;
+	import java.util.Arrays;
+	import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -18,5 +22,26 @@ public class WebAppApplication {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hallo %s! Schön, dass du da bist!", name);
+	}
+
+	@GetMapping("/greet")
+	public String greet(@RequestParam(value = "name", defaultValue = "Stranger") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+	private List<Student> student = new ArrayList<>(Arrays.asList(new Student("Peter", "Meyer")));
+
+	@GetMapping("/greetStudent")
+	public String greetStudent(@RequestParam(defaultValue = "Stranger") String vorname) {
+		return String.format("Hello %s!", vorname);
+	}
+
+	@PostMapping("/join")
+	public String join(@RequestBody Student student) {
+		if (student.vorname == null || student.nachname == null) {
+			return String.format("400 - fehlerhafte Anfrage!");
+		} else {
+			return String.format(student.vorname + " " + student.nachname + " wurde hinzugefügt!" + "\n" + "201 - Ressource wurde erfolgreich erstellt!");
+		}
 	}
 }
